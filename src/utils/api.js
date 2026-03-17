c67c31863709174721ffad1c8b9652d4c80c3e2f
anes255 <anesgaher3000@gmail.com>
2026-03-17 18:54:43 +0100

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://test-kak2.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://test-4ok3.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
